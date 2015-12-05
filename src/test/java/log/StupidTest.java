121b014aaf5f1052995db4d668ac37ec3fc6120e
kolzeq <diego.dupin@gmail.com>
2015-12-05 15:37:01 +0100

    first commit

---
+++ src/test/java/log/StupidTest.java
@@ -0,0 +1,20 @@
+package log;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+
+/**
+ * Created by diego_000 on 05/12/2015.
+ */
+public class StupidTest {
+    private static final Logger log = LoggerFactory.getLogger(StupidTest.class);
+
+    @Test
+    public void test() throws SQLException {
+        Assert.assertEquals(1, 1);
+    }
+}
